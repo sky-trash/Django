0a226da6b640f1ac99f2f49a2cebbb8984dfb17d
Евгений <skytrash118@gmail.com>
2024-09-30 12:20:22 +0400

    Практическая 1

---
+++ practic1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Practic1Config(AppConfig):
+    default_auto_field = 'django.db.models.Auto'
+    name = 'practic1'
---
+++ practic1/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, HttpResponse
+
+def hello(request): 
+    return HttpResponse('Hello world!!!')
